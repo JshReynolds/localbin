a51b0b581743afcb272797b9bb2964233803513d
Josh Reynolds <jreynolds@electrichand.com>
2016-11-25 11:25:20 -0600

    Adding in a couple of pre/postinstall scripts for automating the installation of support sofware

---
+++ postinstall.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+#Install the latest version of node
+nvm install node
+
+#Install creative cloud
+open /usr/local/Caskroom/adobe-creative-cloud/latest/Creative\ Cloud\ Installer.app
+
+#Install the latest version of a local ruby
+rbenv install 2.3.3
+rbenv global 2.3.3
---
+++ preinstall.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+# Brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+mv ~/.zshrc ~/.zshrc.bak
+ln -s ~/etc/shell/zshrc ~/.zshrc
