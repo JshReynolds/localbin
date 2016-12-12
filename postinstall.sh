7c072b5eb288ed1b4673db6b05f95bfe3f30d34c
Josh Reynolds <jreynolds@electrichand.com>
2016-12-12 12:02:40 -0500

    Updating with more gem installations

--- postinstall.sh
+++ postinstall.sh
@@ -3,15 +3,24 @@
 #Install the latest version of node
 . ~/etc/env/enabled/04-nvm
 nvm install node
+npm install -g yarn
+npm install -g react-native-cli
 
 #Install creative cloud
 open /usr/local/Caskroom/adobe-creative-cloud/latest/Creative\ Cloud\ Installer.app
 
-#Install the latest version of a local ruby
+#Install the latest version of a local ruby and cocoapods
 rbenv install 2.3.3
 rbenv global 2.3.3
+rehash
+gem install cocoapods
+gem install xcpretty
+gem install fastlane
 
 cd ~/etc
 git submodule init
 git submodule update
 cd
+
+
+
