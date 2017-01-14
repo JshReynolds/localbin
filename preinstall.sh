03b189e58a7738f02a74e099893a8478ced48743
Josh Reynolds <jreynolds@electrichand.com>
2017-01-14 14:56:48 -0800

    turning brew analytics off

--- preinstall.sh
+++ preinstall.sh
@@ -3,6 +3,8 @@
 # Brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew analytics off
+
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
