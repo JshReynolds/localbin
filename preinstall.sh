31eb8c1ddbc98d80207255c7725504427ae9ce08
Josh Reynolds <jreynolds@electrichand.com>
2017-01-14 18:05:41 -0500

    softlinking zshell theme into oh-my-zsh configured theme dir

--- preinstall.sh
+++ preinstall.sh
@@ -8,6 +8,7 @@ brew analytics off
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+ln -s ~/etc/shell/jshlyd.zsh-theme ~/.oh-my-zsh/themes
 mv ~/.zshrc ~/.zshrc.bak
 ln -s ~/etc/shell/zshrc ~/.zshrc
 
