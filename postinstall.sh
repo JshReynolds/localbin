fc8c200ca8c52d1b2f8a2c6c20da2df8711871ba
Josh Reynolds <jreynolds@electrichand.com>
2019-06-11 15:41:10 -0400

    updating with some environment changes to reflect current use.  Also started using sdkman.

--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,21 @@
 #! /bin/sh
 
 #Install the latest version of node
-. ~/etc/env/enabled/04-nvm
-nvm install node
+. ~/etc/env/enabled/04-nodenv
+nodenv install 12.0.0
+nodenv global 12.0.0
 npm install -g yarn
 npm install -g react-native-cli
+
+#Install java and supporting tools via sdkman
+curl -s "https://get.sdkman.io" | bash
+rehash
+sdk install java
+sdk install scala
+sdk install ant
+sdk install maven
+sdk install gradle
+sdk install leiningen
 
 #Install creative cloud
 open /usr/local/Caskroom/adobe-creative-cloud/latest/Creative\ Cloud\ Installer.app
@@ -13,9 +24,6 @@ open /usr/local/Caskroom/adobe-creative-cloud/latest/Creative\ Cloud\ Installer.
 rbenv install 2.3.3
 rbenv global 2.3.3
 rehash
-gem install cocoapods
-gem install xcpretty
-gem install fastlane
 
 cd ~/etc
 git submodule init
@@ -23,4 +31,8 @@ git submodule update
 cd
 
 
+# turn off apple press n' hold
+defaults write -g ApplePressAndHoldEnabled -bool false
 
+# set a sensible name
+sudo scutil --set HostName laptop
