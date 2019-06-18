fedce242b6c9d79e8b3eb0b7191439d545ed7028
Josh Reynolds <jreynolds@electrichand.com>
2019-06-17 20:26:57 -0400

    Updating with some refinements/improvements

--- postinstall.sh
+++ postinstall.sh
@@ -9,7 +9,7 @@ npm install -g react-native-cli
 
 #Install java and supporting tools via sdkman
 curl -s "https://get.sdkman.io" | bash
-rehash
+. ~/etc/env/enabled/99-sdkman
 sdk install java
 sdk install scala
 sdk install ant
